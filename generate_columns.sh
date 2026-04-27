182b903a2b7fcfe15f45f9f5e0b7bbb93c34bd38
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-04-27 09:48:15 -0500

    spring cleanup

--- generate_columns.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p $1/macros
-echo Generating macros for $5
-dbt run-operation dbt_package_automations.generate_columns_macro --args '{"table_name": "'$5'", "schema_name": "'$4'", "database_name":"'$3'"}' | tail -n +4 | sed 's/\x1b\[[0-9;]*m//g' | sed 's/^[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}  //' > $1/macros/get_$5_columns.sql
--- macros/generate_docs.sql
+++
@@ -1,9 +0,0 @@
-{% macro generate_docs(package) %}
-
-{% set package = ""~ package ~"" %}
-
-{% set zsh_command = "source dbt_packages/dbt_package_automations/generate_docs.sh '../dbt_"""~ package ~""""+"'" %}
-
-{{ log (zsh_command, info=True) }}
-
-{% endmacro %} 
